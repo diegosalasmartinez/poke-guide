6ed99e10b84316f480156f6d8dee6a6f1b63e9c5
Diego Salas <diego.salas@ext.blaze.me>
2021-07-28 22:42:51 -0500

    remove unused files

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react'
+import AppRouter from './routes/AppRouter';
 import './styles/scss/style.scss'
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-const loading = (
-  <div className="pt-3 text-center">
-    <div className="sk-spinner sk-spinner-pulse">
-      a
-    </div>
-  </div>
-)
-
-const Header = React.lazy(() => import('./views/containers/Header'));
-const Pokemons = React.lazy(() => import('./views/pages/pokemons/Pokemons'));
-
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-          <React.Suspense fallback={loading}>
-            <Route path="/" name="Pokemon" render={props => <Header {...props}/>} />
-            <Switch>
-              <Route path="/" name="Pokemon" render={props => <Pokemons {...props}/>} />
-            </Switch>
-          </React.Suspense>
-      </BrowserRouter>
+      <AppRouter></AppRouter>
     );
   }
 }
