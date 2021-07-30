a290bf08cfd4f300b1efc70e453562b4d34642a2
Diego Salas <diego.salas@ext.blaze.me>
2021-07-30 00:50:16 -0500

    resolve warnings

--- src/components/RPagination.js
+++ src/components/RPagination.js
@@ -26,25 +26,25 @@ export default class RPagination extends Component {
                 <Pagination.First disabled={!showFirstPages} onClick={() => this.onClickPage(1)}/>
                 <Pagination.Prev disabled={!showFirstPages} onClick={() => this.onClickPage(indexSelected - 1)}/>
                 {indexSelected - 2 > 0 && 
-                    <Pagination.Item key={0} active={this.state.currentIndex == (indexSelected - 2)} onClick={() => this.onClickPage(indexSelected - 2)}>
+                    <Pagination.Item key={0} active={this.state.currentIndex === (indexSelected - 2)} onClick={() => this.onClickPage(indexSelected - 2)}>
                         {indexSelected - 2}
                     </Pagination.Item>
                 }
                 {indexSelected - 1 > 0 && 
-                    <Pagination.Item key={1} active={this.state.currentIndex == (indexSelected - 1)} onClick={() => this.onClickPage(indexSelected - 1)}>
+                    <Pagination.Item key={1} active={this.state.currentIndex === (indexSelected - 1)} onClick={() => this.onClickPage(indexSelected - 1)}>
                         {indexSelected - 1}
                     </Pagination.Item>
                 }
-                <Pagination.Item key={2} active={this.state.currentIndex == indexSelected} onClick={() => this.onClickPage(indexSelected)}>
+                <Pagination.Item key={2} active={this.state.currentIndex === indexSelected} onClick={() => this.onClickPage(indexSelected)}>
                     {indexSelected}
                 </Pagination.Item>
                 {indexSelected + 1 <= numberPages && 
-                    <Pagination.Item key={3} active={this.state.currentIndex == (indexSelected + 1)} onClick={() => this.onClickPage(indexSelected + 1)}>
+                    <Pagination.Item key={3} active={this.state.currentIndex === (indexSelected + 1)} onClick={() => this.onClickPage(indexSelected + 1)}>
                         {indexSelected + 1}
                     </Pagination.Item>
                 }
                 {indexSelected + 2 <= numberPages && 
-                    <Pagination.Item key={4} active={this.state.currentIndex == (indexSelected + 2)} onClick={() => this.onClickPage(indexSelected + 2)}>
+                    <Pagination.Item key={4} active={this.state.currentIndex === (indexSelected + 2)} onClick={() => this.onClickPage(indexSelected + 2)}>
                         {indexSelected + 2}
                     </Pagination.Item>
                 }
