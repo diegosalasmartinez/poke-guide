e9754c6b7c8d9c2a562ae10d1b29bdb2c9d976ce
Diego Salas <diego.salas@ext.blaze.me>
2021-08-15 19:55:20 -0500

    add styles for cards

--- src/views/pages/pokemons/PokemonCard.js
+++ src/views/pages/pokemons/PokemonCard.js
@@ -19,42 +19,31 @@ export default class PokemonCard extends Component {
         const { pokemon } = this.props;
 
         return (
-            <Col xs="12" sm="6"  md="4" xl="3">
-                <Card>
-                    <Card.Body>
-                        <Row>
-                            <Col>
-                                <Card.Title className="pokemon_card_title">
-                                    <span>
-                                        {capitalize(pokemon.name)}
-                                    </span>
-                                    <Button onClick={this.onClickSeeMore}>
-                                        <Link to={"/pokemons/"+pokemon.name}>
-                                            <FontAwesomeIcon icon={faAngleDoubleRight}/>
-                                        </Link>
-                                    </Button>
-                                </Card.Title>
-                            </Col>
-                        </Row>
-                        <Row className="m-auto">
-                            <Col xs="12" style={{display: "flex"}}>
-                                <Image className="pokemon_prev_img m-auto" src={pokemon.sprites.front_default} width="150px"></Image>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <Card.Text className="mt-2">
-                                    Types:
-                                    {pokemon.types.map(type => 
-                                        <Badge key={type.slot} style={{marginLeft: "5px", backgroundColor: getColorType(type.type.name)}}>
-                                            {type.type.name.toUpperCase()}
-                                        </Badge>  
-                                    )}
-                                </Card.Text>
-                            </Col>
-                        </Row>
-                    </Card.Body>
-                </Card>
+            <Col xs="12" sm="6"  md="4" xl="3" className="pokemon_card">
+                <article className="pokemon_card">
+                    <h2>
+                        <span>
+                            {capitalize(pokemon.name)}
+                        </span>
+                        <div className="types">
+                            {pokemon.types.map(type => 
+                                <Badge key={type.slot} style={{backgroundColor: getColorType(type.type.name)}}>
+                                    {type.type.name.toUpperCase()}
+                                </Badge>  
+                            )}
+                        </div>
+                    </h2>
+                    <div className="pokemon_card_img_container">
+                        <div className="pokemon_card_img">
+                            <Image src={pokemon.sprites.front_default}></Image>
+                        </div>
+                    </div>
+                    <div className="pokemon_card_btn">
+                        <Link to={"/pokemons/"+pokemon.name} onClick={this.onClickSeeMore}>
+                            <FontAwesomeIcon icon={faAngleDoubleRight}/>
+                        </Link>
+                    </div>
+                </article>
             </Col>
         )
     }
