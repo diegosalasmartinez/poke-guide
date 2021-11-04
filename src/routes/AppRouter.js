492325a6a07a789bdf3a8dc122bbeef677464a80
Diego Salas <diesalasmart@gmail.com>
2021-11-03 19:22:15 -0500

    add footer

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -17,27 +17,28 @@ const Header = React.lazy(() => import('../views/common/Header'));
 const Pokedex = React.lazy(() => import('../views/pages/pokedex/Pokedex'));
 const PokemonDetails = React.lazy(() => import('../views/pages/pokemons/PokemonDetails'));
 const Page404 = React.lazy(() => import('../views/pages/404/Page404'));
+const Footer = React.lazy(() => import('../views/common/Footer'));
 
 class AppRouter extends Component {
     render() {        
         return (
           <Container className="bg-main" fluid="xxl">
-          <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-              <BrowserRouter>
-                <React.Suspense fallback={loading}>
+            <Provider store={store}>
+              <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                  <React.Suspense fallback={loading}>
                     <Route path="/" name="Header" render={props => <Header {...props}/>} />
-                  <Switch>
-                      <Route exact path="/" name="Pokedex" render={props => <Pokedex {...props}/>} />
-                      <Route exact path="/pokedex" name="Pokedex" render={props => <Pokedex {...props}/>} />
-                      <Route exact path="/pokedex/:name" name="Pokemon Details" render={props => <PokemonDetails {...props}/>} />
-                      <Route path="/" name="404" render={props => <Page404 {...props}/>} />
-                    {/* </Container> */}
-                  </Switch>
-                </React.Suspense>
-              </BrowserRouter>
-            </PersistGate>
-          </Provider>
+                    <Switch>
+                        <Route exact path="/" name="Pokedex" render={props => <Pokedex {...props}/>} />
+                        <Route exact path="/pokedex" name="Pokedex" render={props => <Pokedex {...props}/>} />
+                        <Route exact path="/pokedex/:name" name="Pokemon Details" render={props => <PokemonDetails {...props}/>} />
+                        <Route path="/" name="404" render={props => <Page404 {...props}/>} />
+                    </Switch>
+                    <Route path="/" name="Footer" render={props => <Footer {...props}/>} />
+                  </React.Suspense>
+                </BrowserRouter>
+              </PersistGate>
+            </Provider>
           </Container>
         )
     }
