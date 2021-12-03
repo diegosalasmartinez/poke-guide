348ebfe68c143c300290525287b4bee23f1eb728
Diego Salas <diesalasmart@gmail.com>
2021-12-03 11:18:45 -0500

    show basic info in en and consider flavor entries abilitis

--- src/views/pages/pokemons/pokemonInfo/PokemonBasicInfo.js
+++ src/views/pages/pokemons/pokemonInfo/PokemonBasicInfo.js
@@ -10,15 +10,15 @@ export default class PokemonBasicInfo extends Component {
         return (
             <Col key={a.slot}>
                 <Col>{capitalize(ability.name)}</Col>
-                <Col style={{fontWeight: '300'}}>{entry.effect}</Col>
+                <Col style={{fontWeight: '300'}}>{entry && entry.effect || ""}</Col>
             </Col>
         )
     }
 
     render() {
-        const { pokemon, version } = this.props;
+        const { pokemon } = this.props;
         const { species } = pokemon;
-        let flavorText = species.flavor_text_entries.find(flavorText => flavorText.language.name === "en" && flavorText.version.name === version);
+        let flavorText = species.flavor_text_entries.find(flavorText => flavorText.language.name === "en");
         flavorText = flavorText ? flavorText.flavor_text : "";
         flavorText = flavorText.replace('', ' ');
 
