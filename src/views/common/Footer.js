1842dccd4bcee2bfb0a68ea06478952e2e248eaa
Diego Salas <diesalasmart@gmail.com>
2021-12-04 22:10:53 -0500

    footer styles

--- src/views/common/Footer.js
+++ src/views/common/Footer.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react'
-import { Row } from 'react-bootstrap'
 
 export default class Footer extends Component {
     render() {
         return (
-            <Row>
-                <div className="footer">Desarrollado por Diego Alejandro Salas Martínez</div>
-            </Row>
+            <div className="footer">Desarrollado por Diego Alejandro Salas Martínez AAA</div>
         )
     }
 }
